2eae18091cd46f49b9f62ea6dac219c4136ff062
Victor Martins <vmartins@doris.mobi>
2022-03-13 16:42:02 -0300

    chore: configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/components/**/*.tsx',
+    './src/pages/**/*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
