350134c2556123e0958d80e2a161bcdebc7c7c7d
Victor Martins <vmartins@doris.mobi>
2022-03-13 22:28:00 -0300

    chore: update migrations

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,18 +3,28 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const main = async () => {
-  const vitu = await prisma.client.upsert({
-    where: {phone: '85997542187'},
-    update: {},
-    create: {
-      name: 'Vitu',
-      phone: '85997542187',
-      sellingPlace: {
-        create: {
-          name: 'Bayusha',
-          location: 'Rua Barão de Aracati,'
-        }
-      }
-    }
-  })
+  // const vitu = await prisma.client.upsert({
+  //   where: { phone: '85997542187' },
+  //   update: {},
+  //   create: {
+  //     name: 'Vitu',
+  //     phone: '85997542187',
+  //     sellingPlaces: {
+  //       create: {
+  //         name: 'Bayhusha'
+  //       }
+  //     }
+  //   }
+  // })
+
+  console.log('seed')
 }
+
+main()
+  .catch((error) => {
+    console.error({ error })
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
